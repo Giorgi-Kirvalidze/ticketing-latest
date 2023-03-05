77d14862bcd9033584eef8f1b0f3d5de124bebca
Zkirvaldize <giorgi.kirvalidze@georgiatech.eu>
2023-03-05 15:39:38 +0400

    .

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -26,7 +26,7 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
-console.log(ticket)
+
     await new TicketCreatedPublisher(natsWrapper.client).publish({
       id: ticket.id,
       title: ticket.title,
